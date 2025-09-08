a8d2f292189cf0312c7d86340173e923fa68c191
ABHAYYADAV30 <abhay830378@gmail.com>
2025-09-08 21:35:01 +0530

    Add files via upload

---
+++ ass3(2).py
@@ -0,0 +1,8 @@
+import math
+num = float(input("Enter a number: "))
+sqrt_val = math.sqrt(num)
+log_val = math.log(num)
+sine_val = math.sin(num)
+print("Square root:", sqrt_val)
+print("Logarithm:", log_val)
+print("Sine:", sine_val)
